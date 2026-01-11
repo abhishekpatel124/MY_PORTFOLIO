df61b188770c3fabda7e1fe550d855534d7c87c2
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-11 21:08:37 +0530

    completed the section1 page

---
+++ src/components/section1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import WhatsAppImage from '../assets/WhatsApp Image 2026-01-08 at 8.07.18 PM.jpeg'
+const Section1 = () => {
+  return (
+    <div className="section1" >
+        <h3>A curious developer who loves turning ideas into interactive web experiences and smart IoT solutions, blending creativity, logic, and continuous learning to build meaningful projects.</h3>
+        <div className="main1">
+      <img src={WhatsAppImage} className="Main_img" alt="WhatsApp Image 2026-01-08 at 8.07.18 PM" />
+      </div>
+    </div>
+  )
+}
+
+export default Section1
