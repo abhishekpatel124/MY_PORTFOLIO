4fb72bbc0099bdd235bfc142ebf3e7a60517c570
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-12 22:24:10 +0530

    added the link and write the css of link

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -5,7 +5,7 @@ import image3 from "../assets/image copy.png";
 function ImageLink() {
   return (
     <>
-  
+  <div className="main-links">
     
     <a
       href="https://www.linkedin.com/authwall?trk=bf&trkInfo=AQGzSRaX4OdzLQAAAZuzExV4u8_5DUF1cSFiEymUpNRaUdXJiZwy1mljVpxk4S4UJoDEAN32h75swCWROq_gBffgmzs2SNxTP0IYw3z9JSeJUirrBLgJnZW3IIupmyBk-TiTKOQ=&original_referer=&sessionRedirect=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fabhishek-patel-3aa289334%3Futm_source%3Dshare_via%26utm_content%3Dprofile%26utm_medium%3Dmember_android"
@@ -15,7 +15,7 @@ function ImageLink() {
       <img
         src={image}
         alt="Clickable"
-        style={{ cursor: "pointer", width: "300px" }}
+        style={{ cursor: "pointer", width: "100px" }}
       />
     </a>
     
@@ -27,7 +27,7 @@ function ImageLink() {
       <img
         src={image2}
         alt="Clickable"
-        style={{ cursor: "pointer", width: "300px" }}
+        style={{ cursor: "pointer", width: "100px" }}
       />
     </a>    
     <a
@@ -38,10 +38,10 @@ function ImageLink() {
       <img
         src={image3}
         alt="Clickable"
-        style={{ cursor: "pointer", width: "300px" }}
+        style={{ cursor: "pointer", width: "100px" }}
       />
     </a>    
-    
+    </div>
     </>
   );
 }
