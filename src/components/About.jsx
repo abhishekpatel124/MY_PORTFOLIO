5c4587ccbbeea1f66d77a20ee07da81188efe3ee
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-14 17:01:21 +0530

    started makeing about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div id="about" className="about-section">
-        <p>I am a passionate web developer with experience in creating dynamic and responsive websites. I enjoy turning ideas into reality using code.</p>
+       
+
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Section1  from './components/section1.jsx'
-import ImageLink from './components/links.jsx'
-import About from './components/About.jsx'  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-   
-    <Section1></Section1>
-     <ImageLink></ImageLink>
-     <About></About>
   </StrictMode>,
 )
