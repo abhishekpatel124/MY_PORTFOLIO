bcf0dd159d89e9792ee4b6b028f48ca52d74146b
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-15 22:06:03 +0530

    made the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <h3>My name is <span>Abhishek Patel</span> Currently pursuing B.Tech in <span>Electronics and Communication Engineering</span>. I am passionate about technology and eager to learn more in this field. I am intrested in<span>
          Web development and IoT</span></h3>
        </div>
-fregb
+
     </div>
   )
 }
