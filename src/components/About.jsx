66e7b588f334bbbefa198403f67881e1dcd87f59
Abhishek Patel <abhishekietian19@gmail.com>
2026-01-15 22:01:58 +0530

    made the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import WhatsAppImage from '../assets/image copy 3.png'
 const About = () => {
   return (
     <div id="about" className="about-section">
-       
+       <h1 id="About">About Me</h1>
+       <div className="main-about">
+        <img src={WhatsAppImage} alt="About me" />
+        <h3>My name is <span>Abhishek Patel</span> Currently pursuing B.Tech in <span>Electronics and Communication Engineering</span>. I am passionate about technology and eager to learn more in this field. I am intrested in<span>
+         Web development and IoT</span></h3>
+       </div>
 
     </div>
   )
